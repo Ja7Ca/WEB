fc8be79b602bac8c26ca871832b4f3434b6c19a0
Ja7Ca <76737729+Ja7Ca@users.noreply.github.com>
2021-10-14 23:58:25 +0700

    Add files via upload

---
+++ Modul 5/index.php
@@ -0,0 +1,39 @@
+<?php
+    require("function.php");
+    
+    $mahasiswa = query("SELECT * from mahasiswa");
+?>
+
+<html>
+<head>
+    <title>Jarot Setiwan L200190247</title>
+</head>
+<body>
+    <h1>Daftar Mahaiswa</h1>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No</th>
+            <th>Aksi</th>
+            <th>NIM</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+            <th>Gambar</th>
+        </tr>
+        <?php $i=1; ?>
+        <?php foreach ($mahasiswa as $row): ?>
+        <tr>
+            <td><?= $i ?>.</td>
+            <td><a href="edit.php">edit</a> | <a href="delete.php">delete</a></td>
+            <td><?= $row["nim"] ?></td>
+            <td><?= $row["nama"] ?></td>
+            <td><?= $row["email"] ?></td>
+            <td><?= $row["jurusan"] ?></td>
+            <td><img src="asset/img/<?= $row["gambar"] ?>" width="50"></td>
+            <?php $i++ ?>
+        </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+</html>
